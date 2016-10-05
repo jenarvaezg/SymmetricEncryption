df97ae011eabbdf706b3275316e1718f8afb548c
jenarvaezg <jenarvaezg@gmail.com>
2016-10-05 10:49:32 +0200

    Simplified the program, need to test

--- src/uc3m/jenarvaezg/dataprot1/SymmetricCipher.java
+++ src/uc3m/jenarvaezg/dataprot1/SymmetricCipher.java
@@ -31,40 +31,50 @@ public class SymmetricCipher {
 	
 	
 	
-	private byte[][] splitIntoBlocks(byte[] input, int size){
+	/*private byte[][] splitIntoBlocks(byte[] input, int size){
 		byte[][] blocks = new byte[size][BLOCK_SIZE];
 		for(int i = 0; i < blocks.length; i++){
 			blocks[i] = Arrays.copyOfRange(input, i*BLOCK_SIZE, i*BLOCK_SIZE + BLOCK_SIZE);
 		}
 		return blocks;
 		
-	}
+	}*/
 	
-	private byte[][] getPaddedBlocks(byte[] input){
-		int diff;
-		if(input.length % BLOCK_SIZE == 0){
-			diff = BLOCK_SIZE;
-		}else{
-			diff = BLOCK_SIZE - (input.length % BLOCK_SIZE);
-		}
+	private byte[] getPaddedInput(byte[] input){
+		int diff = BLOCK_SIZE - (input.length % BLOCK_SIZE);
 		
 		byte[] padding = new byte[diff];
 		for(int i = 0 ; i < diff; i++){
 			padding[i] = (byte)diff;
 		}
 		
-		byte[] padded = new byte[input.length + padding.length];
+		byte[] padded = new byte[input.length + padding.length];	
 		
 		System.arraycopy(input, 0, padded, 0, input.length);
 		System.arraycopy(padding, 0, padded, input.length, padding.length);
 		
-		byte[][] blocks = splitIntoBlocks(padded, padded.length/BLOCK_SIZE);//new byte[][BLOCK_SIZE];
+		return padded;
+		
+		/*byte[][] blocks = splitIntoBlocks(padded, padded.length/BLOCK_SIZE);//new byte[][BLOCK_SIZE];
 
 		
-		return blocks;
+		return blocks;*/
 		
 		
-		
+	}
+	
+	private byte[] removePadding(byte[] padded) throws Exception {
+		int paddingLength = (int)padded[padded.length -1];
+		for(int i = 0; i  < paddingLength; i++){
+			if(padded[(padded.length - 1) - i] != paddingLength){
+				throw new Exception("Padding incorrecto");
+			}
+			
+		}
+		byte[] unpadded = new byte[padded.length - paddingLength];
+		System.arraycopy(padded, 0, unpadded, 0, padded.length - paddingLength);
+		//System.out.println(Arrays.toString(a));
+		return unpadded;
 	}
 
     /*************************************************************************************/
@@ -75,18 +85,20 @@ public class SymmetricCipher {
 		byte[] ciphertext = null;	
 		s = new SymmetricEncryption(byteKey);
 		
-		byte[][] blocks = getPaddedBlocks(input);
+		//byte[][] blocks = getPaddedInput(input);
 		
-		int msgLength = BLOCK_SIZE * blocks.length;
+		byte[] padded = getPaddedInput(input);
+		
+		//int msgLength = BLOCK_SIZE * blocks.length;
 		byte[] prev_ciphered = iv;
 		
-		ciphertext = new byte[msgLength];
+		ciphertext = new byte[padded.length];
 		
 		
-		for(int i = 0; i < blocks.length; i++){
+		for(int i = 0; i < padded.length / BLOCK_SIZE; i++){
 			byte[] xored = new byte[BLOCK_SIZE];
 			for(int j = 0; j < BLOCK_SIZE; j++){	
-				xored[j] = (byte) (blocks[i][j] ^ prev_ciphered[j]);
+				xored[j] = (byte) (padded[i*BLOCK_SIZE + i] ^ prev_ciphered[j]);
 			}
 			prev_ciphered = s.encryptBlock(xored);
 			
@@ -134,19 +146,7 @@ public class SymmetricCipher {
 
 
 
-	private byte[] removePadding(byte[] padded) throws Exception {
-		int paddingLength = (int)padded[padded.length -1];
-		for(int i = 0; i  < paddingLength; i++){
-			if(padded[(padded.length - 1) - i] != paddingLength){
-				throw new Exception("Padding incorrecto");
-			}
-			
-		}
-		byte[] unpadded = new byte[padded.length - paddingLength];
-		System.arraycopy(padded, 0, unpadded, 0, padded.length - paddingLength);
-		//System.out.println(Arrays.toString(a));
-		return unpadded;
-	}
+
 	
 }
 
